d08b578d6309db1e350530523ec08ea8c7cb9d4f
anaysoul <soulyana3@gmail.com>
2023-11-06 19:28:55 -0500

    using & outputting dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+ import logo from '../src/assets/logo.svg'
 import './App.css';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={logo} alt='Stylized atom' />
+      <h1>React Essentials</h1>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
